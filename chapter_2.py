6f7de07129107062a91f8904fae2f2fee565def4
mike.girenko <mike.girenko@cybergrx.com>
2019-02-10 11:57:34 -0700

    Working through chapter 2

--- chapter_2.py
+++ chapter_2.py
@@ -23,3 +23,22 @@ last_name = 'lovelace'
 full_name = first_name + " " + last_name
 print(full_name)
 print('Hello, ' + full_name.title() + '!')
+
+print('\tPython')
+print('Languages:\nPython\nC\nJava')
+
+print('Food:\n\tSpam\n\tEggs\n\tCheese')
+
+has_trailing_spase = 'spam '
+print(has_trailing_spase + '.')
+without_spaces = has_trailing_spase.rstrip()
+print(without_spaces + '.')
+
+has_leading_and_trailing_spaces = ' eggs '
+print('.' + has_leading_and_trailing_spaces + '.')
+print('Without spaces:' + has_leading_and_trailing_spaces.strip() + '.')
+
+message = "Strin'g with apostrophe"
+print(message)
+# message = 'Strin'g with apostrophe in single quotes'
+# print(message)
---
+++ exercises/name_cases.py
@@ -0,0 +1,23 @@
+name = 'Mike'
+print('Hello ' + name + ', would you like to learn Python today?')
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+
+name = 'Mike Duskis'
+print('\t' + name + ' once said, "Make Automation Green Again."')
+
+famous_person = 'Mike Duskis'
+message = '\t' + famous_person + ' once said, "Make Automation Green Again."'
+print(message)
+
+name_with_spaces = ' Matt Jones '
+print('.' + name_with_spaces + '.')
+print('\t' + name_with_spaces)
+print('\n' + name_with_spaces)
+
+print('.' + name_with_spaces.lstrip() + '.')
+print('.' + name_with_spaces.rstrip() + '.')
+print('.' + name_with_spaces.strip() + '.')
