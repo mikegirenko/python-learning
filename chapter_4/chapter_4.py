a1012799e581357d5b5167d9e0be7073b5460357
mike.girenko <mike.girenko@cybergrx.com>
2019-02-17 06:21:02 -0700

    Working through chapter 4

---
+++ chapter_4/chapter_4.py
@@ -0,0 +1,11 @@
+"""
+More working with lists
+"""
+
+food = ['spam', 'eggs', 'chees']
+for item in food:
+    print(item)
+
+# names = ['Tom', 'Mike']
+# for name in names
+#     print(name)
---
+++ chapter_4/pizzas.py
@@ -0,0 +1,16 @@
+my_favorite_pizzas = ['pepperoni', 'double chesse', 'chicken']
+
+for pizza in my_favorite_pizzas:
+    print(pizza)
+
+for pizza in my_favorite_pizzas:
+    print('\tI like ' + pizza)
+
+for pizza in my_favorite_pizzas:
+    print('I like ' + pizza)
+print('Pizza is my favorite food')
+
+animals_list = ['dog', 'cat']
+for animal in animals_list:
+    print('\tA ' + animal + ' would make a great pet')
+print('\tAny of these animals would make a great pet!')
