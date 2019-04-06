2c5d1b0464bf08b912f59c98bdea63ece84df845
mike.girenko <mike.girenko@cybergrx.com>
2019-04-06 09:47:20 -0600

    Working through chapter 9

--- chapter_9/user.py
+++ chapter_9/user.py
@@ -5,6 +5,7 @@ class User:
         self.last_name = last_name
         self.age = age
         self.city = city
+        self.login_attempts = 0
 
     def describe_user(self):
         print('User name is', self.first_name.title(), self.last_name.title(),
@@ -12,6 +13,12 @@ class User:
 
     def greet_user(self):
         print('Hello', self.first_name.title(), self.last_name.title())
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 
 admin = User('jim', 'burnes', '27', 'Denver')
@@ -21,3 +28,10 @@ admin.greet_user()
 admin = User('joe', 'lauzon', '33', 'San Diego')
 admin.describe_user()
 admin.greet_user()
+
+admin = User('sammy', 'flowers', 21, 'Pueblo')
+for i in range(1, 7):
+    admin.increment_login_attempts()
+print('User attempted to login ', admin.login_attempts, 'times')
+admin.reset_login_attempts()
+print('After reset, user attempted to login ', admin.login_attempts, 'times')
