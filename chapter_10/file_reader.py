ef668368e6fe2d68538ddda911d5754a839cfa36
mike.girenko <mike.girenko@cybergrx.com>
2019-04-11 09:54:06 -0600

    Working through chapter 10

---
+++ chapter_10/file_reader.py
@@ -0,0 +1,28 @@
+file = 'learning_python.txt'
+with open(file) as file_object:
+    contents = file_object.read()
+    print(contents)
+
+with open(file) as file_object:
+    for line in file_object:
+        print('\t', line.strip())
+
+string = ''
+with open(file) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    string += line
+print(string)
+
+print('\t', 'File length is', len(lines), 'lines')
+
+if 'task' in string:
+    print('\t', 'File has word "Python"')
+
+string = ''
+with open(file) as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        string += line
+string.replace('Python', 'C')
+print(string)
