d5be397df92c4ed0ae6e024ebb2ea1b02d81f00c
mike.girenko <mike.girenko@cybergrx.com>
2019-11-21 17:08:08 -0700

    Completed next chapter

---
+++ practice_python/draw_a_game_board.py
@@ -0,0 +1,36 @@
+
+"""
+Time for some fake graphics! Let’s say we want to draw game boards that look
+like this:
+
+ --- --- ---
+|   |   |   |
+ --- --- ---
+|   |   |   |
+ --- --- ---
+|   |   |   |
+ --- --- ---
+This one is 3x3 (like in tic tac toe). Obviously, they come in many other sizes
+(8x8 for chess, 19x19 for Go, and many more).
+Ask the user what size game board they want to draw, and draw it for them to
+the screen using Python’s print statement.
+
+"""
+
+
+def main():
+
+    desired_size = input('Enter board size: ')
+    draw_board(desired_size)
+
+
+def draw_board(size):
+    size = int(size)
+    for s in range(size):
+        print(' ---' * size)
+        print('|   ' * (size + 1))
+    print(' ---' * size)
+
+
+if __name__ == '__main__':
+    main()
