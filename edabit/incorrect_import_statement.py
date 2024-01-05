423a14e406d6209f75467ddbec730452ca98a955
mike.girenko <mike.girenko@cybergrx.com>
2024-01-05 13:46:48 -0700

    Completed Incorrect Import Statements

---
+++ edabit/incorrect_import_statement.py
@@ -0,0 +1,18 @@
+"""
+Given a string of an incorrect import statement, return the fixed string
+"""
+
+
+def fix_import(import_statement_to_fix):
+    fixed_import_statement = ["from", "", "import", ""]
+    temp_import_statement = import_statement_to_fix.split(" ")
+    fixed_import_statement[1] = temp_import_statement[3]
+    fixed_import_statement[3] = temp_import_statement[1]
+    fixed_import_statement_string = ' '.join(fixed_import_statement)
+
+    return fixed_import_statement_string
+
+
+print(fix_import("import object from module_name"))
+print(fix_import("import randint from random"))
+print(fix_import("import pi from math"))
