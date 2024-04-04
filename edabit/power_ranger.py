b139be50b5dbb6c6faac787adc14dfad3cd618a6
Mike Girenko <mike.girenko@processunity.com>
2024-04-04 11:23:56 -0600

    Finished Count and Identify Data Types

---
+++ edabit/power_ranger.py
@@ -0,0 +1,25 @@
+"""
+Create a function that takes in n, a, b and returns the number of positive values raised to the nth power that lie in
+the range [a, b], inclusive.
+"""
+
+class PowerRanger:
+
+    def power_ranger(self, n, a, b):
+        number_of_positive_values = 0
+        raised = 0
+        for i in range(1, b):
+            raised = i ** n
+            if a <= raised <= b:
+                number_of_positive_values += 1
+
+        return number_of_positive_values
+
+
+if __name__ == "__main__":
+    obj = PowerRanger()
+    assert obj.power_ranger(2, 49, 65) == 2
+    assert obj.power_ranger(3, 1, 27) == 3
+    assert obj.power_ranger(10, 1, 5) == 1
+    assert obj.power_ranger(5, 31, 33) == 1
+    assert obj.power_ranger(4, 250, 1300) == 3
