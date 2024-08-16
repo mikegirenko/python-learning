abc37f2806ab1bc356b9a0f86969c5f5128f61b7
Mike Girenko <mike.girenko@processunity.com>
2024-08-16 13:37:37 -0600

    Finished First Letter Shift. What a pain : )

--- edabit/first_letter_shift.py
+++ edabit/first_letter_shift.py
@@ -8,17 +8,43 @@ Given a sentence, create a function which shifts the first letter of each word t
 class FirstLetterShift:
     def shift_sentence(self, sentence_in) -> str:
         sentence_out = ""
+        list_of_lists = []
         list_of_words = sentence_in.split(" ")
-        print(list_of_words)
+        for i in list_of_words:
+            list_of_lists.append(list(i))
         i = 0
-        while i < len(list_of_words):
-            f_letter = list_of_words[i][i]
-            list_of_words[i + 1][i] = f_letter
-            print(list_of_words)
+        if len(list_of_lists) == 1:
+            sentence_out = sentence_in
+        else:
+            while i < len(list_of_lists):
+                if i == 0:
+                    f_letter = list_of_words[i][0]
+                    list_of_lists[i + 1][0] = f_letter
+                if i == len(list_of_lists) - 1:
+                    f_letter = list_of_words[len(list_of_words) - 1][0]
+                    list_of_lists[0][0] = f_letter
+                if 0 < i < len(list_of_lists) - 1:
+                    f_letter = list_of_words[i][0]
+                    list_of_lists[i + 1][0] = f_letter
+                i += 1
+
+        list_of_strings = []
+        for i in list_of_lists:
+            list_of_strings.append("".join(i))
+        sentence_out = " ".join(list_of_strings)
 
         return sentence_out
 
 
 if __name__ == "__main__":
     obj = FirstLetterShift()
-    print(obj.shift_sentence("create a function"))
+    assert obj.shift_sentence("create a function") == "freate c aunction"
+    assert (
+        obj.shift_sentence("it should shift the sentence")
+        == "st ihould shift she tentence"
+    )
+    assert (
+        obj.shift_sentence("the output is not very legible")
+        == "lhe tutput os iot nery vegible"
+    )
+    assert obj.shift_sentence("edabit") == "edabit"
