45a3db7464a8de8990898ecfd29cabf45019b181
Mike Girenko <mike.girenko@processunity.com>
2024-09-27 09:36:48 -0600

    Finished Maximum and Minimum Product Triplets

--- edabit/maximum_and_minimum_product_triplets.py
+++ edabit/maximum_and_minimum_product_triplets.py
@@ -5,6 +5,7 @@ One that returns the maximum product of three numbers in a list.
 One that returns the minimum product of three numbers in a list.
 The triplet of an array is a tuple of three elements of different indices, represented by (i, j, k)
 """
+
 import math
 
 
@@ -13,7 +14,9 @@ def max_product(input_list) -> int:
     list_of_triplets = []
     n = len(input_list)
     # Fix the first element as arr[i]
-    for i in range(n - 2): # The range() function returns a sequence of numbers, so, 0, 1, 2
+    for i in range(
+        n - 2
+    ):  # The range() function returns a sequence of numbers, so, 0, 1, 2
         # Fix the second element as arr[j]
         for j in range(i + 1, n - 1):
             # Now look for the third number
@@ -32,7 +35,6 @@ def max_product(input_list) -> int:
     product = triplet_sum
 
     return product
-
 
 
 def min_product(input_list) -> int:
@@ -62,6 +64,6 @@ def min_product(input_list) -> int:
 
 
 assert max_product([-8, -9, 1, 2, 7]) == 504
-# assert max_product([-8, 1, 2, 7, 9]) == 126
-# assert min_product([1, -1, 1, 1]) == -1
-# assert min_product([-5, -3, -1, 0, 4]) == -15
+assert max_product([-8, 1, 2, 7, 9]) == 126
+assert min_product([1, -1, 1, 1]) == -1
+assert min_product([-5, -3, -1, 0, 4]) == -15
