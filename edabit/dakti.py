53bb3da6f6f84c89755ba5421d47bf0dff760e90
Mike Girenko <mike.girenko@processunity.com>
2025-07-01 13:26:54 -0600

    Finished Dakti

--- edabit/dakti.py
+++ edabit/dakti.py
@@ -1,25 +1,41 @@
+import re
+
 """
 Very hard
 First, organize the words based on the numbers they have, then delete the numbers once they are organized.
 """
 
-class Dakiti:
-    def dakiti(self, chorus):
-        orginized_chorus = []
-        split_chorus = chorus.split(" ")
-        for i in range(0, len(split_chorus) - 1):
-            orginized_chorus.append("")
-        print("orginized_chorus", orginized_chorus)
-        #  organize the words based on the numbers they have
-        for i in range(1, len(split_chorus)):
-            for word in split_chorus:
-                if str(i) in word:
-                    orginized_chorus[i - 1] = word
 
-        return orginized_chorus
+class Dakti:
+    def dakiti(self, chorus_in_phrases):
+        chorus_of_the_song = []
+        dict_of_phrases = {}
+        chorus_in_phrases_as_list_no_spaces = []
+        chorus_in_phrases_as_list = chorus_in_phrases.split(" ")
+
+        for item in chorus_in_phrases_as_list:
+            if item != "" and len(item) > 1:
+                chorus_in_phrases_as_list_no_spaces.append(item)
+
+        for phrase in chorus_in_phrases_as_list_no_spaces:
+            this_str = "".join(filter(str.isdigit, phrase))
+            for item in this_str:
+                if item != "":
+                    phrase_index = int(this_str)
+            dict_of_phrases[phrase_index] = phrase.replace(str(phrase_index), "")
+        dict_of_phrases_sorted = sorted(dict_of_phrases.items())
+
+        for item in dict_of_phrases_sorted:
+            chorus_of_the_song.append(item[1])
+        chorus_of_the_song_as_str = " ".join(chorus_of_the_song)
+
+        return chorus_of_the_song_as_str
 
 
 if __name__ == "__main__":
-    obj = Dakiti()
-    print(obj.dakiti("worl2d hell1o "))  # note that trailing space is missing
+    obj = Dakti()
+    print(obj.dakiti("worl2d hell1o"))
     print(obj.dakiti("i2s th1s a3 t4est    "))
+    print(obj.dakiti("yo2u cr3azy a1re ?  "))
+    print(obj.dakiti("en5tere y2a bab1y y3o 4me s6e not7a cuand8o 9me-ves"))
+    print(obj.dakiti("quie3res bebe4r dime1 e5s qu6e qu2e tu7 er8es m9i-bebe"))
