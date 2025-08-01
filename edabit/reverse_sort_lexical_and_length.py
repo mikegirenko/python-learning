3aafad13c9d6e16f2610cd788fae2d87fca05e90
Mike Girenko <mike.girenko@processunity.com>
2025-08-01 13:18:44 -0600

    Started on Reverse Sort: Lexical and Length

---
+++ edabit/reverse_sort_lexical_and_length.py
@@ -0,0 +1,13 @@
+"""
+Very hard
+Write a function that sorts the words in a given string lexicographically (lexical sort) and by length in reverse order.
+"""
+
+class ReverseSortLexicalAndLength:
+    def reverse_sort(self, given_string) -> str:
+        #  continue here
+        pass
+
+if __name__ == "__main__":
+    reverse_sort_object = ReverseSortLexicalAndLength()
+    reverse_sort_object.reverse_sort("You've rocked the pragmatic world in the making!")
